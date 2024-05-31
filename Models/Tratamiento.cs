67a007f4700b50594eb968367ad4da27bda3748e
Wilmar Puerta <wilmarandrespuertajaraillo@gmail.com>
2024-05-31 04:06:07 -0500

    Modificacion del modelo

--- Models/Tratamiento.cs
+++ Models/Tratamiento.cs
@@ -5,7 +5,7 @@ namespace Gestion_de_citas.Models
     public class Tratamiento
     {
         public int Id { get; set; }
-        public string CitaId { get; set; }
+        public int CitaId { get; set; }
         public string Descripcion { get; set; }
         public string Estado { get; set; }
     }
