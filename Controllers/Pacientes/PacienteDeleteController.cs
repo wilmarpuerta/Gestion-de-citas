f725feb605e18f71db129f0ff949e74f08a8f8a1
Wilmar Puerta <wilmarandrespuertajaraillo@gmail.com>
2024-05-30 21:16:44 -0500

    Implementando el eliminar a pacientes (Delete)

--- Controllers/Pacientes/PacienteDeleteController.cs
+++ Controllers/Pacientes/PacienteDeleteController.cs
@@ -1,9 +1,32 @@
 
 
+using Gestion_de_citas.Models;
+using Gestion_de_citas.Services.Pacientes;
+using Microsoft.AspNetCore.Mvc;
+
 namespace Gestion_de_citas.Controllers.Pacientes
 {
-    public class PacienteDeleteController
+    [ApiController]
+    [Route("api/[controller]")]
+    public class PacienteDeleteController : ControllerBase
     {
-        
+        private readonly IPacientesRepository _pacientesRepository;
+
+        public PacienteDeleteController(IPacientesRepository pacientesRep)
+        {
+            _pacientesRepository = pacientesRep;
+        }
+
+        [HttpPut("Eliminar/{id}")]
+        public IActionResult PacienteDelete(int id)
+        {
+            try{
+                _pacientesRepository.DeletePaciente(id);
+                return Ok("Paciente eliminado correctamente");
+            }
+            catch{
+                return BadRequest("Error al eliminar pacirnte");
+            }
+        }
     }
 }
