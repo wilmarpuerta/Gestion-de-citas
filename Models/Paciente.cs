f59abea944c2eae17f6abf2afd262aa9a668ac8e
Wilmar Puerta <wilmarandrespuertajaraillo@gmail.com>
2024-05-30 21:12:41 -0500

    Cambios al modelo

--- Models/Paciente.cs
+++ Models/Paciente.cs
@@ -14,11 +14,7 @@ namespace Gestion_de_citas.Models
         public string Correo { get; set; }
         public string Telefono { get; set; }
         public string Direccion { get; set; }
-        public EstadoPaciente Estado { get; set; }
+        public string Estado { get; set; }
     }
-    public enum EstadoPaciente
-        {
-            Activo,
-            Inactivo
-        }
+    
 }
