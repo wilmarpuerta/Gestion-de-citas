766636c70a10d75af34e902e1917c1c063cd0e06
Wilmar Puerta <wilmarandrespuertajaraillo@gmail.com>
2024-05-30 21:08:09 -0500

    Quitando parseo de estados

--- Data/BaseContext.cs
+++ Data/BaseContext.cs
@@ -11,18 +11,6 @@ namespace Gestion_de_citas.Data
 
         }
 
-         protected override void OnModelCreating(ModelBuilder modelBuilder)
-        {
-            modelBuilder.Entity<Paciente>()
-                .Property(p => p.Estado)
-                .HasConversion(
-                    v => v.ToString(),
-                    v => (EstadoPaciente)Enum.Parse(typeof(EstadoPaciente), v)
-                );
-
-            base.OnModelCreating(modelBuilder);
-        }
-
         public DbSet<Cita> Citas { get; set; }
         public DbSet<Paciente> Pacientes { get; set; }
         public DbSet<Medico> Medicos { get; set; }
